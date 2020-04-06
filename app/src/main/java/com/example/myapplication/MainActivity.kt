fc37665813a7db80a3e624070fe61118901c3fc6
Mona Lisa <duong.anh.tuanb@sun-asterisk.com>
2020-04-06 17:56:06 +0700

    aaaaa

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        aaaaaaaaa
         setContentView(R.layout.activity_main)
     }
 }
